92f9166189bb9a98213bec08d499b8ae7618336a
Antoine <antoine.laborderie@gmail.com>
2017-05-11 10:40:48 +0200

    Compatible with ngc

--- bundle/ad-dfp.js
+++ bundle/ad-dfp.js
@@ -1,50 +1,39 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
-var http_1 = require("@angular/http");
-require("rxjs/add/operator/map");
-var AdDFPComponent = (function () {
-    function AdDFPComponent(_elementRef, _http) {
+const core_1 = require('@angular/core');
+const http_1 = require('@angular/http');
+require('rxjs/add/operator/map');
+class AdDFPComponent {
+    constructor(_elementRef, _http) {
         this._elementRef = _elementRef;
         this._http = _http;
     }
-    AdDFPComponent.prototype.ngOnInit = function () {
-        var _this = this;
-        this.getSettings().subscribe(function (data) {
-            _this.settings = data;
-            _this.defineAds(_this.settings, googletag);
-            var tag = _this.getTag(_this.type, _this.settings);
-            if (_this.settings.adBlockDetector) {
-                _this.detectAdBlocker();
+    ngOnInit() {
+        this.getSettings().subscribe(data => {
+            this.settings = data;
+            this.defineAds(this.settings, googletag);
+            let tag = this.getTag(this.type, this.settings);
+            if (this.settings.adBlockDetector) {
+                this.detectAdBlocker();
             }
-            if (_this.type === "hidden_inter") {
+            if (this.type === "hidden_inter") {
             }
             else {
-                _this.displayAd(tag);
+                this.displayAd(tag);
             }
         });
-    };
-    AdDFPComponent.prototype.detectAdBlocker = function () {
+    }
+    detectAdBlocker() {
         if (typeof isAdBlockEnabled === 'undefined') {
             this.showDetectedAdBlocker();
         }
-    };
-    AdDFPComponent.prototype.showDetectedAdBlocker = function () {
+    }
+    showDetectedAdBlocker() {
         if (typeof adsFunction !== 'undefined') {
             adsFunction();
         }
-    };
-    AdDFPComponent.prototype.getTag = function (type, settings) {
-        var tag;
+    }
+    getTag(type, settings) {
+        let tag;
         if (type == "banner") {
             tag = settings.tags.banner;
         }
@@ -55,16 +44,16 @@ var AdDFPComponent = (function () {
             tag = 0;
         }
         return tag;
-    };
-    AdDFPComponent.prototype.displayAd = function (tag) {
+    }
+    displayAd(tag) {
         document.getElementsByClassName(this.type)[0].setAttribute("id", "div-gpt-ad-" + tag + "-0");
         if (googletag && googletag.apiReady) {
             googletag.cmd.push(function (result) {
                 googletag.display('div-gpt-ad-' + tag + '-0');
             });
         }
-    };
-    AdDFPComponent.prototype.defineAds = function (settings, googletag) {
+    }
+    defineAds(settings, googletag) {
         var googletag = googletag || {};
         googletag.cmd = googletag.cmd || [];
         var gptAdSlots = [];
@@ -74,33 +63,39 @@ var AdDFPComponent = (function () {
                 addSize([728, 400], [settings.mapping.tablet.width, settings.mapping.tablet.height]).
                 addSize([1024, 400], [settings.mapping.desktop.width, settings.mapping.desktop.height]).
                 build();
-            gptAdSlots[0] = googletag.defineSlot("/" + settings.network + "/" + settings.ID.banner, [[settings.mapping.mobile.width, settings.mapping.mobile.height], [settings.mapping.tablet.width, settings.mapping.tablet.height], [settings.mapping.desktop.width, settings.mapping.desktop.height]], "div-gpt-ad-" + settings.tags.banner + "-0").
+            gptAdSlots[0] = googletag.defineSlot(`/${settings.network}/${settings.ID.banner}`, [[settings.mapping.mobile.width, settings.mapping.mobile.height], [settings.mapping.tablet.width, settings.mapping.tablet.height], [settings.mapping.desktop.width, settings.mapping.desktop.height]], `div-gpt-ad-${settings.tags.banner}-0`).
                 defineSizeMapping(mappingBanner).
                 addService(googletag.pubads());
-            gptAdSlots[1] = googletag.defineOutOfPageSlot("/" + settings.network + "/" + settings.ID.inter, "div-gpt-ad-" + settings.tags.inter + "-0")
+            gptAdSlots[1] = googletag.defineOutOfPageSlot(`/${settings.network}/${settings.ID.inter}`, `div-gpt-ad-${settings.tags.inter}-0`)
                 .addService(googletag.pubads());
             googletag.pubads().enableSingleRequest();
             googletag.pubads().collapseEmptyDivs();
             googletag.enableServices();
         });
-    };
-    AdDFPComponent.prototype.getSettings = function () {
+    }
+    getSettings() {
         return this._http.get('/assets/settings/settings.json')
-            .map(function (response) { return response.json(); });
-    };
-    return AdDFPComponent;
-}());
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", String)
-], AdDFPComponent.prototype, "type", void 0);
-AdDFPComponent = __decorate([
-    core_1.Component({
-        selector: 'ad-dfp',
-        template: "\n        <div class=\"adContainer\" style=\"text-align: center;\">\n            <div class={{type}}>\n\n            </div>\n        </div>\n    "
-    }),
-    __metadata("design:paramtypes", [typeof (_a = typeof core_1.ElementRef !== "undefined" && core_1.ElementRef) === "function" && _a || Object, typeof (_b = typeof http_1.Http !== "undefined" && http_1.Http) === "function" && _b || Object])
-], AdDFPComponent);
+            .map(response => response.json());
+    }
+}
+AdDFPComponent.decorators = [
+    { type: core_1.Component, args: [{
+                selector: 'ad-dfp',
+                template: `
+        <div class="adContainer" style="text-align: center;">
+            <div class={{type}}>
+
+            </div>
+        </div>
+    `
+            },] },
+];
+AdDFPComponent.ctorParameters = () => [
+    { type: core_1.ElementRef, },
+    { type: http_1.Http, },
+];
+AdDFPComponent.propDecorators = {
+    'type': [{ type: core_1.Input },],
+};
 exports.AdDFPComponent = AdDFPComponent;
-var _a, _b;
 //# sourceMappingURL=ad-dfp.js.map
