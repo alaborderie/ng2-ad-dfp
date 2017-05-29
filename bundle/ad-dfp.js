af9aae036090e8a5c307892cc71cb36d8a898759
Antoine <antoine.laborderie@gmail.com>
2017-05-29 09:52:41 +0200

    Fixed http request for settings

--- bundle/ad-dfp.js
+++ bundle/ad-dfp.js
@@ -1,8 +1,17 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 const core_1 = require('@angular/core');
 const http_1 = require('@angular/http');
 require('rxjs/add/operator/map');
-class AdDFPComponent {
+let AdDFPComponent = class AdDFPComponent {
     constructor(_elementRef, _http) {
         this._elementRef = _elementRef;
         this._http = _http;
@@ -78,28 +87,26 @@ class AdDFPComponent {
         });
     }
     getSettings() {
-        return this._http.get('/assets/settings/settings.json')
+        return this._http.get('assets/settings/settings.json')
             .map(response => response.json());
     }
-}
-AdDFPComponent.decorators = [
-    { type: core_1.Component, args: [{
-                selector: 'ad-dfp',
-                template: `
+};
+__decorate([
+    core_1.Input(), 
+    __metadata('design:type', String)
+], AdDFPComponent.prototype, "type", void 0);
+AdDFPComponent = __decorate([
+    core_1.Component({
+        selector: 'ad-dfp',
+        template: `
         <div class="adContainer" style="text-align: center;">
             <div class={{type}}>
 
             </div>
         </div>
     `
-            },] },
-];
-AdDFPComponent.ctorParameters = () => [
-    { type: core_1.ElementRef, },
-    { type: http_1.Http, },
-];
-AdDFPComponent.propDecorators = {
-    'type': [{ type: core_1.Input },],
-};
+    }), 
+    __metadata('design:paramtypes', [core_1.ElementRef, http_1.Http])
+], AdDFPComponent);
 exports.AdDFPComponent = AdDFPComponent;
 //# sourceMappingURL=ad-dfp.js.map
